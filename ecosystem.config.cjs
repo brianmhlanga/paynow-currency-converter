d1b99881e955e0a5579b8b9d9be55154a63cdf5e
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-04-24 06:33:38 +0200

    port change

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: 'Paynow Currency Converter',
-        port: '3017',
+        port: '3021',
         exec_mode: 'cluster',
         instances: '1',
         script: './.output/server/index.mjs' 
