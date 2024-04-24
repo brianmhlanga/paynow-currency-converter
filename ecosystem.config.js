b856b1fe48b846cfdacee5b0daeeac4121a3ee27
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-04-24 06:25:28 +0200

    init

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [
+      {
+        name: 'Paynow Currency Converter',
+        port: '3017',
+        exec_mode: 'cluster',
+        instances: '1',
+        script: './.output/server/index.mjs' 
+      }
+    ]
+  }
