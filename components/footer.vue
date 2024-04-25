6cd0c3e7f6fc97be0e9ffd231e9edb385a6763f5
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-04-25 14:42:50 +0200

    updated logo

--- components/footer.vue
+++ components/footer.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="surface-section px-4 py-6 footer  md:px-6 lg:px-8 text-center">
-      <img src="https://www.paynow.co.zw/blog/wp-content/themes/paynowblog/assets/img/logo-white.svg" alt="Image" height="50">
+      <span> <NuxtImg src="https://www.paynow.co.zw/blog/wp-content/themes/paynowblog/assets/img/logo-white.svg" alt="Image" height="40" class="toplogo" /></span><span class="logo-text footer"> | Currency Converter</span>
       <!-- <div class="font-medium text-900 mt-4 mb-3">© 2024 Al</div> -->
     </div>
 </template>
@@ -8,5 +8,8 @@
 .footer {
     margin-top: 80px;
     background-color: #2563eb !important;
+}
+span.logo-text.footer {
+    color: white !important;
 }
 </style>
